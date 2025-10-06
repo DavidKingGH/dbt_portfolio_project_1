299d8fd8a8aeac519a1a065864743a56480489a2
David.King <David-King@live.com>
2025-10-06 19:05:42 -0400

    Updated typecasting and labeling of source data.

--- models/bronze/dim_items.sql
+++ models/bronze/dim_items.sql
@@ -1,2 +1,25 @@
+with source as (
 SELECT *
 FROM {{ source('ga4_ecommerce_data', 'dim_items')}}
+),
+
+renamed_and_casted as (
+SELECT
+
+    cast(loaded_at as timestamp) as loaded_at,
+    cast(item_id as integer) as item_id,
+	
+    item_name,
+	item_brand,
+	item_variant,
+	item_category,
+	item_category2,
+	item_category3,
+	item_category4,
+	item_category5
+
+FROM source
+)
+
+select *
+from renamed_and_casted
--- models/bronze/fct_events.sql
+++ models/bronze/fct_events.sql
@@ -1,2 +1,41 @@
+with source as (
 SELECT *
 FROM {{ source('ga4_ecommerce_data', 'fact_events')}}
+),
+
+renamed_and_casted as (
+select
+	event_id,
+	CAST(strptime(event_date, '%Y%m%d') AS date) as event_date,
+	event_ts as event_timestamp,
+	event_name,
+	user_pseudo_id as user_id,
+	ga_session_id,
+	page_location,
+	page_referrer,
+	engagement_time_msec,
+	gclid,
+	utm_source,
+	utm_medium,
+	utm_campaign,
+	utm_term,
+	utm_content,
+	transaction_id,
+	event_value_in_usd,
+	source,
+	medium,
+	campaign_name,
+	continent,
+	country,
+	region,
+	city,
+	device_category,
+	operating_system,
+	browser,
+	mobile_brand_name,
+	loaded_at
+from source
+)
+
+select *
+from renamed_and_casted
