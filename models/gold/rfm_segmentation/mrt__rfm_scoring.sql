24f355486ac34b271388b5a6d27caad14b9352af
David.King <David-King@live.com>
2025-10-14 19:00:35 -0400

    Updated type casting for mart rfm_segmentation models.

--- models/gold/rfm_segmentation/mrt__rfm_scoring.sql
+++ models/gold/rfm_segmentation/mrt__rfm_scoring.sql
@@ -1,10 +1,10 @@
 with scored as (
   select
+    as_of_date::date as as_of_date, 
     user_id, 
     recency::int as recency, 
     frequency::int as frequency, 
     monetary::decimal as monetary,
-    as_of_date::date as as_of_date, 
     ntile(5) over (partition by as_of_date order by recency asc)::int as   r_score,
     ntile(5) over (partition by as_of_date order by frequency desc)::int as f_score,
     ntile(5) over (partition by as_of_date order by monetary  desc)::int as m_score
