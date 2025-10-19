a275467fdcd33b74b6e38255e2b59b12809734fa
David.King <David-King@live.com>
2025-10-19 16:33:00 -0400

    Updated predictive models to reference users from dim_users model.

--- models/gold/predictive_features/mrt__item_interactions_last_30d.sql
+++ models/gold/predictive_features/mrt__item_interactions_last_30d.sql
@@ -1,62 +1,63 @@
 with 
 
+report_date as (
+
+select 
+    max(event_timestamp) as as_of_timestamp
+from {{ ref('fct_events')}}
+),
+
+
 dim_items_deduplicated as (
 
 select * from (
     select *, row_number() over(partition by item_id order by item_category) as rn
     from {{ ref('dim_items') }}
-) where rn = 1
+) x where rn = 1
 
 ),
 
-report_date as (
-
-select 
-    max(event_timestamp)::date as as_of_date
-from {{ ref('fct_events')}}
+events_last_30d as (
+select fe.*
+from {{ ref('fct_events')}} fe
+cross join report_date a 
+where fe.event_timestamp <= a.as_of_timestamp
+    and fe.event_timestamp >= a.as_of_timestamp - interval '29' day
 ),
 
 item_interactions_last_30d as (
 select
-    bfe.user_id, 
-    bfe.ga_session_id, 
-    bfe.event_name, 
-    bfe.event_timestamp, 
+    fe.user_id, 
+    fe.ga_session_id,
+    fe.event_id,
+    fe.event_name, 
+    fe.event_timestamp, 
     bfei.item_id, 
     bfei.price, 
     bfei.quantity,
-    coalesce(did.item_category, 'unknown') as item_category,
-    a.as_of_date
-from {{ ref('fct_events')}}      bfe
-join {{ ref('fct_event_items')}} bfei on bfe.event_id = bfei.event_id
+    coalesce(did.item_category, 'unknown') as item_category
+from events_last_30d fe
+join {{ ref('fct_event_items')}} bfei using(event_id)
 left join dim_items_deduplicated did on did.item_id = bfei.item_id
-cross join report_date a
-where bfe.event_timestamp <= a.as_of_date 
-and bfe.event_timestamp >= a.as_of_date - interval '29' day
 ),
 
-sessions_with_checkout_30d as (
-select 
-    distinct fe.ga_session_id
-from {{ ref('fct_events')}} fe
-cross join report_date a 
-where fe.event_name = 'begin_checkout'
-    and fe.event_timestamp <= a.as_of_date
-    and fe.event_timestamp >= a.as_of_date - interval '29' day
+sessions_with_purchase_30d as (
+  select distinct ga_session_id
+  from events_last_30d
+  where event_name = 'purchase'
 ),
 
-int_last_item_category as (
+last_item_category as (
   select
     user_id,
     item_category as last_carted_category_last_30d,
-    row_number() over (partition by user_id order by event_timestamp desc) as rn
+    row_number() over (partition by user_id order by event_timestamp desc, event_id desc) as rn
   from item_interactions_last_30d
   where event_name = 'add_to_cart'
 ),
 
-int_user_product_interest_features_last_30d as (
+user_product_interest_features_last_30d as (
 select 
-    max(as_of_date) as as_of_date,
     user_id, 
 
     -- counts 
@@ -77,22 +78,30 @@ select
 
 
 from item_interactions_last_30d ii
-left join sessions_with_checkout_30d sp on ii.ga_session_id = sp.ga_session_id
+left join sessions_with_purchase_30d sp on ii.ga_session_id = sp.ga_session_id
 group by user_id
 ),
 
 final as (
 
 select
-    pf.as_of_date::date as as_of_date,
-    pf.user_id,
-    (items_carted::double) / nullif(items_viewed,0) as cart_to_view_ratio_last_30d, 
-    abandoned_cart_value_last_30d::decimal as abandoned_cart_value_last_30d,
-    avg_price_of_items_carted_last_30d::decimal as avg_price_of_items_carted_last_30d,
-    last_carted_category_last_30d::varchar as last_carted_category_last_30d,
-    (items_viewed::double) / nullif(item_category_viewed,0) as specialist_ratio_last_30d
-from int_user_product_interest_features_last_30d pf 
-left join int_last_item_category ic on ic.user_id = pf.user_id 
+    a.as_of_timestamp::date as as_of_date,
+    du.user_id,
+    case when pf.items_viewed > 0
+         then (pf.items_carted::double) / pf.items_viewed
+         else null 
+    end as cart_to_view_ratio_last_30d, 
+    pf.abandoned_cart_value_last_30d::decimal as abandoned_cart_value_last_30d,
+    pf.avg_price_of_items_carted_last_30d::decimal as avg_price_of_items_carted_last_30d,
+    ic.last_carted_category_last_30d::varchar as last_carted_category_last_30d,
+    case when pf.item_category_viewed > 0
+         then (pf.items_viewed::double) / item_category_viewed 
+         else null 
+    end as views_per_category_last_30d
+from {{ ref('dim_users') }} du
+cross join report_date a
+left join user_product_interest_features_last_30d pf on du.user_id = pf.user_id 
+left join last_item_category ic on du.user_id = ic.user_id 
 and ic.rn = 1
 )
 
